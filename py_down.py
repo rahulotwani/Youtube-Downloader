c583d10bbe928ce2fd78f03ff01aebf4b50674f0
root <rahulotwani7@gmail.com>
2019-03-11 01:25:32 +0530

    initial commit

---
+++ py_down.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+import random
+link=input('Please Enter the URL of the video -- \n')
+try: 
+	yt = YouTube(link)
+except: 
+	print("Connection Error")
+stream = yt.streams.filter(file_extension='mp4').all()[2]
+try: 
+	stream.download()
+except: 
+	print("Error while Downloading... Please Try Again !! \n")
