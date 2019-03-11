90cef174e51aa80b5574343fd210664a6bf81c1b
root <rahulotwani7@gmail.com>
2019-03-11 17:37:57 +0530

    corrected errors

--- py_down.py
+++ py_down.py
@@ -7,12 +7,8 @@ except:
 	print("Connection Error")
 stream = yt.streams.filter(file_extension='mp4').all()[2]
 try: 
-	print("Starting to download : " )
-	print(yt.title)
 	stream.download()
-	yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
-    	yt.streams.all()
-   	print("Dowmload is Complete\n")
-	
+	print("Successfully downloaded : ")
+	print(yt.title)
 except: 
 	print("Error while Downloading... Please Try Again !! \n")
