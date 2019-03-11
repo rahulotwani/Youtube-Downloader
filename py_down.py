c99c94f1131ae516703337124005d45fbcf9432a
Rahul Otwani <rahulotwani7@gmail.com>
2019-03-11 17:27:30 +0530

    Update py_down.py

--- py_down.py
+++ py_down.py
@@ -11,8 +11,8 @@ try:
 	print(yt.title)
 	stream.download()
 	yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
-    yt.streams.all()
-    print("Dowmload is Complete\n")
+    	yt.streams.all()
+   	print("Dowmload is Complete\n")
 	
 except: 
 	print("Error while Downloading... Please Try Again !! \n")
