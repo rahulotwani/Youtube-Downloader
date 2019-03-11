0c1ac65381f340b9f9b249050955bbb0af2d319b
root <rahulotwani7@gmail.com>
2019-03-11 10:18:55 +0530

    added directory for travis

---
+++ Youtube-Downloader/py_down.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+import random
+link=input('Please Enter the URL of the video -- \n')
+try: 
+	yt = YouTube(link)
+except: 
+	print("Connection Error")
+stream = yt.streams.filter(file_extension='mp4').all()[2]
+try: 
+	print("Starting to download : " )
+	print(yt.title)
+	stream.download()
+	yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
+    yt.streams.all()
+    print("Dowmload is Complete\n")
+	
+except: 
+	print("Error while Downloading... Please Try Again !! \n")
